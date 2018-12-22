c4b686acc850d8a076efd95d79b8db96712f0056
Martin Köhler <mjkoehler1@gmx.de>
2018-12-22 21:22:22 +0100

    mk22122019

---
+++ getlink.py
@@ -0,0 +1,5 @@
+import re, urllib.request
+htmlSource = urllib.request.urlopen("http://www.spiegel.de").read(200000).decode('utf-8')
+linksList = re.findall('<a href=(.*?)>.*?</a>',htmlSource)
+for link in linksList:
+    print(link)
