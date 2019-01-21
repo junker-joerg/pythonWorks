db3c5922583999c21ab878f1f4479aa0f4725932
Köhler <MKoehler1@kpmg.com>
2019-01-21 13:40:25 +0100

    ...13:40

--- NLP_CIO_Tool3.py
+++ NLP_CIO_Tool3.py
@@ -2,4 +2,7 @@
 # ! git push https://github.com/junker-joerg/pythonworks [jeden Abend]
 # ! Das Beste aus den Vorversionen NLP_CIO_Tool2.py,  NLP_CIO_Tool.py...
 # ! ... corpusclean.py und CorpUniC01.py übernehmen
-# !   
+# ToDO: über alle Dateien - aus jeder Datei wird mit PDFminer der Text gezogen und ín eine neue Datei kopiert   
+# ToDO: in einem zweiten Schritt werden die .txt-Dateien durch die Cleaner-Funktionen geschickt und dann wird
+# ToDO: die REIN-Datei für den Corpus geschrieben - es wird im LogFile vermerkt, wieviel Sätze / Worte geschrieben
+# ToDO: wurden
