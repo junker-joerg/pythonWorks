78d3c73c9a444fc9b91e95980f015eda91e67124
Köhler <MKoehler1@kpmg.com>
2019-01-21 13:34:39 +0100

    Dritter Versuch - der wird es nun

---
+++ NLP_CIO_Tool3.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# ! git push https://github.com/junker-joerg/pythonworks [jeden Abend]
+# ! Das Beste aus den Vorversionen NLP_CIO_Tool2.py,  NLP_CIO_Tool.py...
+# ! ... corpusclean.py und CorpUniC01.py übernehmen
+# !   
