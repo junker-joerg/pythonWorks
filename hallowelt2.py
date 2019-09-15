1ac582bf6db965ca86a6a1450c26dbb9272ac5d9
Martin Köhler <mjkoehler1@gmx.de>
2019-09-15 18:43:07 +0200

    16.9.2019 Abends

---
+++ hallowelt2.py
@@ -0,0 +1,15 @@
+# -*- coding=utf-8 -*-
+class classname(object):
+    def funcname(parameter_list):
+        pass
+    pass
+
+def testfnktMK():
+    print("Hallo Welt")
+    
+def main():
+    testfnktMK()
+
+if __name__ == '__main__':
+    main()
+    
