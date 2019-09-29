06160ec4fe45e5e881fd7385e7695c389f09cbaf
Martin Köhler <mjkoehler1@gmx.de>
2019-09-29 17:02:10 +0200

    17:01

--- hallowelt2.py
+++ hallowelt2.py
@@ -7,6 +7,19 @@ def main():
     a = 1
     testfnktMK()
 
+for i in range(10):
+    print(i)
+else:
+    print("Ende")
+
+
+class MK_CLASS(object):
+    """docstring for MK_CLASS."""
+    def __init__(self, arg):
+        super(MK_CLASS, self).__init__()
+        self.arg = arg
+        
+
 if __name__ == '__main__':
     a = 12
     main()
