0bbe0cf08650377ce3635e978058429570610d0d
Martin Köhler <junker-joerg@users.noreply.github.com>
2019-11-21 13:36:27 +0100

    geändert auf dem iPad

--- getlink.py
+++ getlink.py
@@ -1,3 +1,4 @@
+# geändert auf dem iPad mit Pythonista 3
 import re, urllib.request
 htmlSource = urllib.request.urlopen("http://www.spiegel.de").read(200000).decode('utf-8')
 linksList = re.findall('<a href=(.*?)>.*?</a>',htmlSource)
