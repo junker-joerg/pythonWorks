1dc8558a2b71c90132a69a82459e35055460a743
Martin Köhler <junker-joerg@users.noreply.github.com>
2020-04-19 18:57:21 +0200

    25.12

--- corpUniC01Test.py
+++ corpUniC01Test.py
@@ -3,7 +3,7 @@ from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk import Text
 import nltk
-import unicodecsv
+#import unicodecsv
 mytext = u"Das ist Satz 1. Satz zwei enthält öüÖÄÜ Umlaute. Satz 3 ist der letzte Satz!"
 type(mytext)
 tokens = nltk.word_tokenize(mytext, language='german')
