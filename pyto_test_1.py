ee2ec2de453923336f21b56be59d9ffc0157c7b5
Martin Köhler <junker-joerg@users.noreply.github.com>
2020-08-19 15:51:47 +0200

    v3

--- pyto_test_1.py
+++ pyto_test_1.py
@@ -7,14 +7,14 @@ from __future__ import print_function, division
 
 import numpy as np
 
-
+'''
 with np.printoptions(threshold=5, edgeitems=4):
     print(np.random.random(10))
-    
+'''    
   
   
 file_contents = ""
-with open("pyto_test.txt", "r") as file_handle:
+with open("pyto_test.txt", "r") as file_handle:# Permission error - nur local
     for line in file_handle.readlines():
         file_contents += line.replace("-", "_")  
         
